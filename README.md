86a874e6dacf6b5b9bc5be2fae863b2bcdab1ac9
Fedor Masor <masor.dev@gmail.com>
2021-10-05 16:58:19 +0300

    nullable types

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ## 🔠 Basic syntax
 - 1️⃣ Hello World ✔️
 - 2️⃣ Variables ✔️
-- 3️⃣ Nullable Types
+- 3️⃣ Nullable Types ✔️
 - 4️⃣ Conditional Statement if
 - 5️⃣ Conditional Statement when
 - 6️⃣ Collections
-- 7️⃣ Сycles
+- 7️⃣ Cycles
 - 8️⃣ Functions
 - 9️⃣ Exceptions
 ## 🔠Functional programming
---
+++ src/nullabletypes/NullableTypes.kt
@@ -0,0 +1,14 @@
+package nullabletypes
+
+
+//Дано:
+private var st1: String? = "null"
+private var st2: String? = "k"
+private var st3: String? = null
+
+//Задача: в переменную result сохранить сумму длин всех строк и вывести результат в консоль.
+
+fun main() {
+    val result = (st1?.length ?: 0) + (st2?.length ?: 0) + (st3?.length ?: 0)
+    println(result)
+}

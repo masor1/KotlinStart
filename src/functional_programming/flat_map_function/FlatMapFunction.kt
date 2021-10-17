5a6a8ac2e23b1f896655ab1d6065c82a41815774
Fedor Masor <masor.dev@gmail.com>
2021-10-17 23:52:18 +0300

    flat map function

--- src/functional_programming/flat_map_function/FlatMapFunction.kt
+++ src/functional_programming/flat_map_function/FlatMapFunction.kt
@@ -6,8 +6,8 @@ fun main() {
         "file2" to listOf(35, -20, 45, 15, 12),
         "file3" to listOf(15, 40, 45, 15, 72)
     )
-    val average1 = data.filter { it.value.all { it >= 0 }}.flatMap { it.value }.average()
-    val average2 = data.filterNot { it.value.any { it < 0 }}.flatMap { it.value }.average()
+    val average1 = data.filter { it -> it.value.all { it >= 0 }}.flatMap { it.value }.average()
+    val average2 = data.filterNot { it -> it.value.any { it < 0 }}.flatMap { it.value }.average()
 
     println(average1)
     println(average2)

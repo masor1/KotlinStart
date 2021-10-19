34cd7498051c2f3aef7aad5f922be7cb8d773b13
Fedor Masor <masor.dev@gmail.com>
2021-10-19 20:16:37 +0300

    high order fun create

--- src/functional_programming/HigherOrderFunctionsСreate.kt
+++ src/functional_programming/HigherOrderFunctionsСreate.kt
@@ -3,10 +3,16 @@ package functional_programming
 import java.util.*
 
 fun main() {
-    val res = modifyString("каеееееефф!!!") { it.uppercase(Locale.getDefault()) }
-    println(res)
+    val resString = modifyString("каеееееефф!!!") { it.uppercase(Locale.getDefault()) }
+    println(resString)
+
+    val resCollection = modifyCollection(listOf(1,4,6,78,67,23,12,0,1,1,0,45)) { println(it.sum()) }
 }
 
-fun modifyString(string: String, modify: (String) -> String): String {
+inline fun modifyString(string: String, modify: (String) -> String): String {
     return modify(string)
+}
+
+inline fun modifyCollection(collection: List<Int>, modify: (List<Int>) -> Unit) {
+    return modify(collection)
 }

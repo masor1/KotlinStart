88f2c10601d374d54742504931cdcbecd4f654e2
Fedor Masor <masor.dev@gmail.com>
2021-10-24 14:33:06 +0300

    constructors

--- src/oop/constructors/Country.kt
+++ src/oop/constructors/Country.kt
@@ -1,4 +1,11 @@
 package oop.constructors
 
 class Country {
+    var name: String
+    var population: Long
+
+    constructor(name: String, population: Long) {
+        this.name = name
+        this.population = population
+    }
 }

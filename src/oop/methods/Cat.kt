b804bafaddc1e45e3fc1c755d572c3660e5728e4
Fedor Masor <masor.dev@gmail.com>
2021-10-26 12:53:31 +0300

    methods

--- src/oop/methods/Cat.kt
+++ src/oop/methods/Cat.kt
@@ -1,5 +1,12 @@
 package oop.methods
 
-class Cat(val name: String, val age: Int, val weight: Float = 0f) {
+class Cat(val name: String, var age: Int, var weight: Float = 0f) {
+
+    val isOld: Boolean
+    get() = age >= 12
+
+    fun printInfo() {
+        println("Кличка: $name Возраст: $age Вес: $weight")
+    }
 
 }
---
+++ src/oop/methods/Main.kt
@@ -0,0 +1,7 @@
+package oop.methods
+
+fun main() {
+    val cat = Cat(name = "Barsik", 5, 3f)
+    cat.printInfo()
+    println(cat.isOld)
+}

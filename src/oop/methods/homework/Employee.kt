74e3087a01132920d267d76c3a34721e00e2598f
Fedor Masor <masor.dev@gmail.com>
2021-10-26 13:24:31 +0300

    methods HW

--- src/oop/methods/homework/Employee.kt
+++ src/oop/methods/homework/Employee.kt
@@ -1,4 +1,14 @@
 package oop.methods.homework
 
-class Employee {
+import java.time.Year
+
+class Employee(var name: String, var position: String, var yearOfEmployment: Int) {
+
+    val experience: Int
+    get() = Year.now().value - yearOfEmployment
+
+    fun work() {
+        println("Работаю...")
+    }
+
 }
--- src/oop/methods/homework/Main.kt
+++ src/oop/methods/homework/Main.kt
@@ -1,5 +1,17 @@
 package oop.methods.homework
 
 fun main() {
+    val employee = Employee("Fedor", "Android developer", 2021)
 
+    employee.work()
+    employee.showInfo()
 }
+
+fun Employee.showInfo() = println(
+            "Employee(" +
+            "name: $name, " +
+            "position: $position, " +
+            "yearOfEmployment: $yearOfEmployment, " +
+            "experience: $experience" +
+            ")"
+)

e9b318b6652072dc632839ee4bd2f3d794e4a988
Fedor Masor <masor.dev@gmail.com>
2021-10-29 15:40:26 +0300

    data_class

--- src/oop/data_class/Main.kt
+++ src/oop/data_class/Main.kt
@@ -1,5 +1,16 @@
 package oop.data_class
 
 fun main() {
+    val student1 = Student("Fedor", "Turkov", "0")
+    val student2 = Student("Fedor", "Turkov", "0")
+
+    println(student1)
+    println(student2)
+
+    println(student1.hashCode())
+    println(student2.hashCode())
+
+    println(student1 == student2)
+    println(student1 === student2)
 
 }

368f572b36396cd60b54f720141b60878937b7ac
Fedor Masor <masor.dev@gmail.com>
2021-11-02 15:42:26 +0300

    anonymous_classes optimization

--- src/oop/anonymous_classes/homework/Main.kt
+++ src/oop/anonymous_classes/homework/Main.kt
@@ -2,9 +2,15 @@ package oop.anonymous_classes.homework
 
 fun main() {
     val sportsmanship = Sportsmanship()
-    sportsmanship.callWaterboy(object : Waterboy{
+
+    sportsmanship.callWaterboy(object : Waterboy {
         override fun bringWater() {
-            println("Несу воду...")
+            "Принес воду"
         }
+
     })
+
+    sportsmanship.callWaterboy {
+        println("Принес воду")
+    }
 }
--- src/oop/anonymous_classes/homework/Sportsmanship.kt
+++ src/oop/anonymous_classes/homework/Sportsmanship.kt
@@ -1,7 +1,13 @@
 package oop.anonymous_classes.homework
 
 class Sportsmanship {
+
     fun callWaterboy(waterboy: Waterboy) {
         waterboy.bringWater()
     }
+
+    inline fun callWaterboy(bringWater: () -> Unit) {
+        return bringWater()
+    }
+
 }

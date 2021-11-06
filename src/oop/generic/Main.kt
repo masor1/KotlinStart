b5d02412c53de0a3388b6962241719f6d0f20205
Fedor Masor <masor.dev@gmail.com>
2021-11-06 17:55:26 +0300

    generic

---
+++ src/oop/generic/Main.kt
@@ -0,0 +1,6 @@
+package oop.generic
+
+fun main() {
+    val list = MyArrayList()
+    println(list.get(0))
+}
--- src/oop/generic/MyArrayList.kt
+++ src/oop/generic/MyArrayList.kt
@@ -1,4 +1,50 @@
 package oop.generic
 
-class MyArrayList {
+import java.lang.IndexOutOfBoundsException
+
+
+class MyArrayList : MyList{
+
+    private var array = arrayOfNulls<String>(10)
+    private var size = 0
+
+    override fun get(index: Int): String {
+        if (index in 0 until size) {
+            array[index]?.let {
+                return it
+            }
+        }
+        throw IndexOutOfBoundsException()
+    }
+
+    override fun add(string: String) {
+        if (size == array.size - 1) {
+            array = array.copyOf(array.size * 2)
+        }
+        array[size] = string
+        size++
+    }
+
+    override fun remove(element: String) {
+        for ((index, string) in array.withIndex()) {
+            if (string == element) {
+                removeAt(index = index)
+                return
+            }
+        }
+    }
+
+    override fun removeAt(index: Int) {
+        if (index in 0 until size) {
+            for (i in index until size - 1) {
+                array[i] = array[i + 1]
+            }
+            size--
+        } else {
+            throw IndexOutOfBoundsException()
+        }
+    }
+
+    override fun size() = size
+
 }

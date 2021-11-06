b6ad1ad6d895123f1e8e9eba7fc20c01249331dd
Fedor Masor <masor.dev@gmail.com>
2021-11-06 18:06:50 +0300

    generic

--- src/oop/generic/Main.kt
+++ src/oop/generic/Main.kt
@@ -2,5 +2,15 @@ package oop.generic
 
 fun main() {
     val list = MyArrayList()
-    println(list.get(0))
+    list.add("AAA")
+    list.add("BBb")
+    list.add("CcC")
+    list.add("dDd")
+    list.add("eeE")
+    list.removeAt(4)
+    list.remove("BBb")
+
+    for (i in 0 until list.size()) {
+        println(list.get(i))
+    }
 }
